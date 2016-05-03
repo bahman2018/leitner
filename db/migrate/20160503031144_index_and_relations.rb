ed171366085560d5b1a27687b649fd8aa35d0f09
Bahman <info@bahman.ws>
2016-05-02 20:53:28 -0700

    DataBase

---
+++ db/migrate/20160503031144_index_and_relations.rb
@@ -0,0 +1,14 @@
+class IndexAndRelations < ActiveRecord::Migration
+  def slef.up
+    change_column("users","email", string, :limite=>100)
+    rename_cloumn("users","password","hashed_password")
+    add_index("words","word")
+    add_index("users","username")
+  end
+  def slef.down
+    change_column("users","email", string, :default=>"", :null=>false)
+    rename_column("users","hashed_password","password")
+    remove_index("users","username")
+    remove_index("words","word")
+  end
+end
