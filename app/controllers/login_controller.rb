db0ee9c413e6606a86740b2cf37327bf0d837f11
Bahman <info@bahman.ws>
2016-05-05 11:41:21 -0700

    05052016

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -7,6 +7,11 @@ class LoginController < ApplicationController
     render('sing_up')
   end
   def create
-
+    @user=User.new(params[:user])
+    if @user.save
+      redirect_to(:action => 'index')
+    else
+      render('sing_up')
+    end
   end
 end
